8b790fb6decc788f219dfc49b261e53c7b7662e0
Jamie Brindle <jamie.brindle7.work@gmail.com>
2020-10-26 02:02:41 +0000

    Init

---
+++ TopLevelFilesToIndividualFolders/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace TopLevelFilesToIndividualFolders
+{
+    public class Program
+    {
+        static void Main()
+        {
+            foreach (var file in Directory.GetFiles("."))
+            {
+                if (!file.Contains("TopLevelFilesToIndividualFolders.exe"))
+                {
+                    Console.WriteLine($"Processing: {file}");
+
+                    Directory.CreateDirectory(Path.GetFileNameWithoutExtension(file));
+                    File.Move(file, Path.GetFileNameWithoutExtension(file) + "\\" + Path.GetFileName(file));
+                }
+            }
+        }
+    }
+}
